d2c883f9c307bf0e686a62aa636bdf9b3bac41b8
TSHEGOFATSO <mojelatshego@gmail.com>
2018-01-11 09:56:55 +0200

    login and main page
    login and main page

--- Envelope/Envelope/HomePage.xaml.cs
+++ Envelope/Envelope/HomePage.xaml.cs
@@ -16,5 +16,35 @@ namespace Envelope
 		{
 			InitializeComponent ();
 		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_5(object sender, EventArgs e)
+        {
+
+        }
     }
 }
