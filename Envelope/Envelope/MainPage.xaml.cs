5ff4e86d3900b70f513aa2c51dad8873e29a323a
the1Spartacus <thulanianesh@gmail.com>
2018-01-26 08:13:26 +0200

    login

--- Envelope/Envelope/MainPage.xaml.cs
+++ Envelope/Envelope/MainPage.xaml.cs
@@ -4,21 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Auth;
+
+
 
 namespace Envelope
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-
-        async void loginClicked(object sender, EventArgs args)
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void LoginClicked(object sender, EventArgs args)
         {
             await Navigation.PushAsync(new HomePage());
         }
+        private async void LoginWithFacebook_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FacebookProfilePage());
+        }
 
-  
     }
+
 }
